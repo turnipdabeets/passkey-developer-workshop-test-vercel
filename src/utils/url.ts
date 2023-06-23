8e3aa4dd884d88ae6e768cbbdaeb61b1da2b0d59
Anna <anna.garcia@beyondidentity.com>
2023-06-23 15:26:27 -0400

    url fix

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,27 +1,7 @@
 export function authUrl(): URL {
-  switch (process.env.REGION) {
-    case "us": {
-      return new URL("https://auth-us.beyondidentity.com");
-    }
-    case "eu": {
-      return new URL("https://auth-eu.beyondidentity.com");
-    }
-    default: {
-      throw new Error(`Unsupported or unrecognized region: ${process.env.REGION}`);
-    }
-  }
+  return new URL('https://auth-us.beyondidentity.com');
 }
 
 export function apiUrl(): URL {
-  switch (process.env.REGION) {
-    case "us": {
-      return new URL("https://api-us.beyondidentity.com");
-    }
-    case "eu": {
-      return new URL("https://api-eu.beyondidentity.com");
-    }
-    default: {
-      throw new Error(`Unsupported or unrecognized region: ${process.env.REGION}`);
-    }
-  }
+  return new URL('https://api-us.beyondidentity.com');
 }
